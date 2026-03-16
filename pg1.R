42c0ea872039da75e30648fb98da301708966ad2
clairef82 <claire.formont@insee.fr>
2026-03-16 10:28:10 +0000

    Initialisation du projet

---
+++ pg1.R
@@ -0,0 +1,6 @@
+an <-25 # millésime du bilan
+an2<-26 # année finale
+anm<-24 # millésime mariages (en général n-1)
+
+# dossier global
+chemin <- paste0("V:/EAPR/Bilans_demographiques/Bilan_demo_20",an)
